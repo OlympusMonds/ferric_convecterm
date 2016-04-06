81dddcf680a258a78db72c95b28fc359a1f28e77
Luke Mondy <luke.s.mondy@gmail.com>
2016-04-06 12:33:56 +1000

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let xmin : f64 = 0.;
+    let xmax : f64 = 2.;
+    let ymin : f64 = 0.;
+    let ymax : f64 = 2.;
+
+    let nx : f64 = 21.;  // These should be unsigned ints, but rust is
+    let ny : f64 = 21.;  // toooo explicit
+
+    let dx = ( xmax - xmin ) / nx;
+    let dy = ( ymax - ymin ) / ny;
+
+    println!("dx = {}\ndy = {}\n", dx, dy);
+
+    let mut u : [f64; nx];
 }
