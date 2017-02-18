8899d7dc824b882b6d78565814ba31d88df94015
Luke Mondy <luke.s.mondy@gmail.com>
2017-02-18 15:49:29 +1100

    commit before mega change

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+
 struct BasicContext {
     xmin : f64,
     xmax : f64,
@@ -16,16 +17,114 @@ struct BasicContext {
 
 struct Context {
     u   : Vec<f64>,  // vel in x
+    un  : Vec<f64>,  // vel in x
     v   : Vec<f64>,  // vel in y
+    vn  : Vec<f64>,  // vel in y
     p   : Vec<f64>,  // pressure
+    pn  : Vec<f64>,  // pressure
     rho : Vec<f64>,  // density
     nu  : Vec<f64>,  // viscosity
     t   : Vec<f64>,  // temp
+    tn  : Vec<f64>,  // temp
     k   : Vec<f64>,  // conductivity
 }
 
 
-fn print_field(bc : BasicContext, field : Vec<f64>) {
+fn apply_vel_bc(bc : &BasicContext, c : & mut Context) {
+    let mut idx : usize;
+    let mut off : usize;
+
+    for j in 0..bc.ny {
+        idx = (bc.nx * j) as usize;
+
+        // left wall free-slip
+        c.u[idx] = 0.;
+        c.v[idx] = c.v[idx + 1];
+
+        // right wall free-slip
+        off = idx + (bc.nx - 1) as usize;
+        c.u[off] = 0.;
+        c.v[off] = c.v[off - 1];
+    }
+
+    for i in 0..bc.nx {
+        // bottom wall free-slip
+        c.u[i as usize] = c.u[(bc.nx + i) as usize];
+        c.v[i as usize] = 0.;
+
+        // top wall free-slip
+        idx = (bc.nx * (bc.ny - 1) + i) as usize;
+        c.u[idx] = c.u[(bc.nx * (bc.ny - 2) + i) as usize];
+        c.v[idx] = 0.;
+    }
+}
+
+
+fn apply_thermal_bc(bc : &BasicContext, c : & mut Context) {
+    let mut idx : usize;
+
+    for j in 0..bc.ny {
+        idx = (bc.nx * j) as usize;
+
+        // left wall, no temp escape
+        c.t[idx] = c.t[idx + 1];
+
+        // right wall, no temp escape
+        let off = idx + (bc.nx - 1) as usize;
+        c.t[off] = c.t[off - 1];
+    }
+
+    for i in 0..bc.nx {
+        // bottom wall 
+        c.v[i as usize] = 1000.;
+
+        // top wall
+        idx = (bc.nx * (bc.ny - 1) + i) as usize;
+        c.v[idx] = 0.;
+    }
+}
+
+
+fn solve_advection_diffusion(bc : &BasicContext, c : & mut Context) {
+    let mut kx : f64;
+    let mut ky : f64;
+
+    let dx = ( bc.xmax - bc.xmin ) / (bc.nx as f64);  // TODO put this in the context
+    let dy = ( bc.ymax - bc.ymin ) / (bc.ny as f64);
+    let dx2 = dx * dx;
+    let dy2 = dy * dy;
+    let twodx = 2. * dx;
+    let twody = 2. * dy;
+
+    for i in 0..(bc.nx * bc.ny) {
+        c.tn[i as usize] = c.t[i as usize];
+    }
+
+    let mut m : usize;  // middle
+    let mut n : u32;  // north
+    let mut e : usize;
+    let mut s : usize;
+    let mut w : usize;  // west
+
+    for j in 0..bc.ny {
+        for i in 0..bc.nx {
+            m = (bc.nx * j + i) as usize;
+            n = (bc.nx * (j-1) + i);
+            s = (bc.nx * (j+1) + i) as usize;
+            e = (bc.nx * j + (i+1)) as usize;
+            w = (bc.nx * j + (i-1)) as usize;
+
+            kx = c.k[m] + (c.tn[e] - 2. * c.tn[m] + c.tn[w]) / dx2;
+            ky = c.k[m] + (c.tn[s] - 2. * c.tn[m] + c.tn[n]) / dx2;
+
+        }
+    }
+
+    apply_thermal_bc(bc, c);
+}
+
+
+fn print_field(bc : &BasicContext, field : & Vec<f64>) {
     for jj in 0..bc.ny {
         let j = bc.ny - jj - 1;
         for i in 0..bc.nx {
@@ -46,7 +145,7 @@ fn main() {
                            ymax   : 2.,
                            nx     : 21,
                            ny     : 21, 
-
+ 
                            cp     : 60.,   
                            H      : 0.,    
                            dt     : 9e-5,  
@@ -61,11 +160,15 @@ fn main() {
     println!("dx = {}\ndy = {}\nele = {}", dx, dy, ele);
 
     let mut c = Context{ u   : vec![0.      as f64; ele as usize],  // vel in x
+                         un  : vec![0.      as f64; ele as usize],
                          v   : vec![0.      as f64; ele as usize],
+                         vn  : vec![0.      as f64; ele as usize],
                          p   : vec![0.      as f64; ele as usize],
+                         pn  : vec![0.      as f64; ele as usize],
                          rho : vec![bc.refrho;      ele as usize],
                          nu  : vec![1.      as f64; ele as usize],
                          t   : vec![500.    as f64; ele as usize],
+                         tn  : vec![500.    as f64; ele as usize],
                          k   : vec![100.    as f64; ele as usize],
                         };
 
@@ -78,16 +181,28 @@ fn main() {
             let idx = (bc.nx * j + i) as usize;
             c.p[idx] = bc.refrho * dy as f64 * bc.g * (bc.ny - j) as f64;  // Approx lithostatic pressure
             // Apply thermal initial state
-            /*
-            if j < 4 && i < nx/2 {
-                t[idx] = 1000.;
+            if j < 4 && i < bc.nx/2 {
+                c.t[idx] = 1000.;
             }
-            else if j > ny-5 && i > nx/2 {
-                t[idx] = 0.;
+            else if j > bc.ny-5 && i > bc.nx/2 {
+                c.t[idx] = 0.;
             }
-            */
         }
     }
+    print_field(&bc, &c.p);
 
-    print_field(bc, c.p);
+    apply_vel_bc(&bc, & mut c);
+    apply_thermal_bc(&bc, & mut c);
+
+    // Main loop
+    loop {
+
+        if timestep > 100 {
+            break
+        }
+
+        solve_advection_diffusion(&bc, & mut c);
+
+        timestep += 1;
+    }
 }
